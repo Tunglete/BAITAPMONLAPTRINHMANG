98d0203ab06796b673eb6862acf32d4474a6f4ef
DESKTOP-3FJLN9U\Tung_le_te <thanhtung.white@gmail.com>
2017-02-11 10:27:54 +0700

    front-end asp.net mvc 5 (start-project)

---
+++ WebDienThoaiDiDong/WebDienThoaiDiDong/Areas/Admin/Controllers/DangNhapController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDienThoaiDiDong.Areas.Admin.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        // GET: Admin/DangNhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
