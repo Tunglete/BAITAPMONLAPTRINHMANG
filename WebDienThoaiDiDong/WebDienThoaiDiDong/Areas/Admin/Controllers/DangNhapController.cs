2273eeed8a71a7777b917dfc3fc11d2b9892ea0c
DESKTOP-3FJLN9U\Tung_le_te <thanhtung.white@gmail.com>
2017-02-19 16:45:24 +0700

    Lắp giao diện admin,thêm file js,css, thêm file backup CSDL.bak

--- WebDienThoaiDiDong/WebDienThoaiDiDong/Areas/Admin/Controllers/DangNhapController.cs
+++ WebDienThoaiDiDong/WebDienThoaiDiDong/Areas/Admin/Controllers/DangNhapController.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebDienThoaiDiDong.Models;
 
 namespace WebDienThoaiDiDong.Areas.Admin.Controllers
 {
     public class DangNhapController : Controller
     {
+        QuanLyBanDienThoaiEntities db = new QuanLyBanDienThoaiEntities();
         // GET: Admin/DangNhap
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult DangNhap()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string username = f["name"];
+            string password = f["pass"];
+            ADMIN admin = db.ADMINs.SingleOrDefault(n => n.TenDangNhap == username && n.MatKhau == password);
+            if (admin!=null)
+            {
+                ViewBag.TrangThai = "Đăng nhập thành công";
+                Session["TaiKhoan"] = admin;
+                return View();
+            }
+            else
+            {
+                ViewBag.TrangThai = "Tên tài khoản hoặc mật khẩu không đúng";
+                return View();
+            }
+            
         }
     }
 }
---
+++ WebDienThoaiDiDong/WebDienThoaiDiDong/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDienThoaiDiDong.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: Admin/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DoanhSoPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult ThongKeDuLieuPartial()
+        {
+            return PartialView();
+        }
+    }
+}
